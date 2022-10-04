84293e6e1f3666ed87ead6d048e3cbc3b598d471
Nitish Kumar <nk98721000@gmail.com>
2022-10-04 19:10:34 +0530

    bug fix

--- app/src/main/java/com/rotiking/delivery/models/Food.java
+++ app/src/main/java/com/rotiking/delivery/models/Food.java
@@ -3,32 +3,66 @@ package com.rotiking.delivery.models;
 import java.io.Serializable;
 
 public class Food implements Serializable {
+    private boolean available;
+    private String by;
+    private String description;
+    private int discount;
     private String food_id;
+    private String food_includes;
+    private String food_type;
+    private String ingredients;
     private String name;
     private String photo;
-    private String description;
-    private String food_includes;
-    private String ingredients;
-    private String food_type;
-    private boolean available;
     private int price;
-    private int discount;
     private double rating;
 
     public Food() {}
 
-    public Food(String food_id, String name, String photo, String description, String food_includes, String ingredients, String food_type, boolean available, int price, int discount, double rating) {
+    public Food(boolean available, String by, String description, int discount, String food_id, String food_includes, String food_type, String ingredients, String name, String photo, int price, double rating) {
+        this.available = available;
+        this.by = by;
+        this.description = description;
+        this.discount = discount;
         this.food_id = food_id;
+        this.food_includes = food_includes;
+        this.food_type = food_type;
+        this.ingredients = ingredients;
         this.name = name;
         this.photo = photo;
-        this.description = description;
-        this.food_includes = food_includes;
-        this.ingredients = ingredients;
-        this.food_type = food_type;
-        this.available = available;
         this.price = price;
-        this.discount = discount;
         this.rating = rating;
+    }
+
+    public boolean isAvailable() {
+        return available;
+    }
+
+    public void setAvailable(boolean available) {
+        this.available = available;
+    }
+
+    public String getBy() {
+        return by;
+    }
+
+    public void setBy(String by) {
+        this.by = by;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getDiscount() {
+        return discount;
+    }
+
+    public void setDiscount(int discount) {
+        this.discount = discount;
     }
 
     public String getFood_id() {
@@ -37,6 +71,30 @@ public class Food implements Serializable {
 
     public void setFood_id(String food_id) {
         this.food_id = food_id;
+    }
+
+    public String getFood_includes() {
+        return food_includes;
+    }
+
+    public void setFood_includes(String food_includes) {
+        this.food_includes = food_includes;
+    }
+
+    public String getFood_type() {
+        return food_type;
+    }
+
+    public void setFood_type(String food_type) {
+        this.food_type = food_type;
+    }
+
+    public String getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(String ingredients) {
+        this.ingredients = ingredients;
     }
 
     public String getName() {
@@ -55,60 +113,12 @@ public class Food implements Serializable {
         this.photo = photo;
     }
 
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public String getFood_includes() {
-        return food_includes;
-    }
-
-    public void setFood_includes(String food_includes) {
-        this.food_includes = food_includes;
-    }
-
-    public String getIngredients() {
-        return ingredients;
-    }
-
-    public void setIngredients(String ingredients) {
-        this.ingredients = ingredients;
-    }
-
-    public String getFood_type() {
-        return food_type;
-    }
-
-    public void setFood_type(String food_type) {
-        this.food_type = food_type;
-    }
-
-    public boolean isAvailable() {
-        return available;
-    }
-
-    public void setAvailable(boolean available) {
-        this.available = available;
-    }
-
     public int getPrice() {
         return price;
     }
 
     public void setPrice(int price) {
         this.price = price;
-    }
-
-    public int getDiscount() {
-        return discount;
-    }
-
-    public void setDiscount(int discount) {
-        this.discount = discount;
     }
 
     public double getRating() {
--- app/src/main/java/com/rotiking/delivery/models/Topping.java
+++ app/src/main/java/com/rotiking/delivery/models/Topping.java
@@ -3,32 +3,50 @@ package com.rotiking.delivery.models;
 import java.io.Serializable;
 
 public class Topping implements Serializable {
-    private String topping_id;
+    private boolean available;
+    private String by;
+    private String food_includes;
     private String name;
     private String photo;
-    private String food_includes;
-    private boolean available;
     private int price;
     private double rating;
+    private String topping_id;
 
     public Topping() {}
 
-    public Topping(String topping_id, String name, String photo, String food_includes, boolean available, int price, double rating) {
-        this.topping_id = topping_id;
+    public Topping(boolean available, String by, String food_includes, String name, String photo, int price, double rating, String topping_id) {
+        this.available = available;
+        this.by = by;
+        this.food_includes = food_includes;
         this.name = name;
         this.photo = photo;
-        this.food_includes = food_includes;
-        this.available = available;
         this.price = price;
         this.rating = rating;
-    }
-
-    public String getTopping_id() {
-        return topping_id;
-    }
-
-    public void setTopping_id(String topping_id) {
         this.topping_id = topping_id;
+    }
+
+    public boolean isAvailable() {
+        return available;
+    }
+
+    public void setAvailable(boolean available) {
+        this.available = available;
+    }
+
+    public String getBy() {
+        return by;
+    }
+
+    public void setBy(String by) {
+        this.by = by;
+    }
+
+    public String getFood_includes() {
+        return food_includes;
+    }
+
+    public void setFood_includes(String food_includes) {
+        this.food_includes = food_includes;
     }
 
     public String getName() {
@@ -47,22 +65,6 @@ public class Topping implements Serializable {
         this.photo = photo;
     }
 
-    public String getFood_includes() {
-        return food_includes;
-    }
-
-    public void setFood_includes(String food_includes) {
-        this.food_includes = food_includes;
-    }
-
-    public boolean isAvailable() {
-        return available;
-    }
-
-    public void setAvailable(boolean available) {
-        this.available = available;
-    }
-
     public int getPrice() {
         return price;
     }
@@ -77,5 +79,13 @@ public class Topping implements Serializable {
 
     public void setRating(double rating) {
         this.rating = rating;
+    }
+
+    public String getTopping_id() {
+        return topping_id;
+    }
+
+    public void setTopping_id(String topping_id) {
+        this.topping_id = topping_id;
     }
 }
